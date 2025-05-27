18c4b8606fe639eda13dfc19268a5b19ee79a90c
Elena Nazarova <nazzarova.in@gmail.com>
2025-05-27 09:05:23 +0300

    Add authentication users

--- logo_extractor/urls.py
+++ logo_extractor/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('website.urls')),
     path('api/', include('logosfinder.urls')),
+    path('api/', include('user.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
